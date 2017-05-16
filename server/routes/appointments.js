caa603426a4cc2c1795469aa5a8ea2d8c838f593
Anna Springfield <anna.e.springfield@gmail.com>
2017-05-15 20:16:26 -0500

    add route scaffolding

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -4,11 +4,34 @@ var passport = require('passport');
 var path = require('path');
 
 /**
-  * @api {get} /appointments/existing/:user_id
-  * @apiDescription Get a user's existing appointments
+  * @api {get} /appointments/existing/:user_id Get User Appointments
+  * @apiVersion 0.1.0
+  * @apiName GetUserAppointments
   * @apiGroup Appointments
+  * @apiDescription Get a user's existing appointments
+
   * @apiParam {Number} user_id User's unique ID
   * @apiSuccess {Object[]} appointments   Array of appointment objects
+  * @apiSuccess {Number} appointments.id   Unique ID of appointment
+  * @apiSuccess {Object} appointments.client   Object with information about
+      the client for whom the appointment was made
+  * @apiSuccess {String} appointments.client.first   First name of client
+  * @apiSuccess {String} appointments.client.last   Last name of client
+  * @apiSuccess {String} appointments.client.address   Address of client
+  * @apiSuccess {Object} appointments.info   Object with information about the
+      appointment
+  * @apiSuccess {Number} appointments.info.appointment_number   Number to identify
+      appointment in Bridging's separate database
+  * @apiSuccess {String} appointments.info.start_time   Start time of appointment
+  * @apiSuccess {String} appointments.info.end_time   End time of appointment
+  * @apiSuccess {String} appointments.info.appointment_type   Type of appointment
+      ("shopping" or "new bed")
+  * @apiSuccess {String} appointments.info.location_name   "Roseville" or "Bloomington"
+  * @apiSuccess {String} appointments.info.location_address   Address of location
+  * @apiSuccess {Date} appointments.info.date  Date of appointment
+  * @apiSuccess {Date} appointments.info.delivery_date  Date of delivery (if applicable)
+  * @apiSuccess {String} appointments.info.delivery_date    Status of appointment
+      ("confirmed" or "pending")
   * @apiSuccessExample {json} Success-Response:
       HTTP/1.1 200 OK
       [{
@@ -18,7 +41,7 @@ var path = require('path');
           "last": "Tolliver",
           "address": "1400 Lizard Ln, St. Paul, MN"
         },
-        "appointment_info": {
+        "info": {
           "appointment_number" : 4590,
           "start_time": "9:15 am",
           "end_time": "10:15 am",
@@ -34,15 +57,18 @@ var path = require('path');
   * @apiErrorExample {json} List error
   *    HTTP/1.1 500 Internal Server Error
 */
-router.get('/existing/:user_id', {
+router.get('/existing/:user_id', function(req, res) {
   // TODO: add code
-})
+});
 
 /**
-  * @api {get} /appointments/available
+  * @api {get} /appointments/available Get Available Appointments
+  * @apiVersion 0.1.0
+  * @apiName GetAvailableAppointments
+  * @apiGroup Appointments
   * @apiDescription Get appointment objects that match the parameters and still
     have available spots left
-  * @apiGroup Appointments
+
   * @apiParam {Date} min_date   Earliest date in range to be checked
   * @apiParam {Date} max_date   Latest date in range to be checked
   * @apiParam {String} appointment_type   Type of appointment ("shopping" or "new bed")
@@ -50,11 +76,22 @@ router.get('/existing/:user_id', {
   * @apiParam {Number} location_id  Unique ID of location (database ID for
                                     Bloomington or for Roseville)
   * @apiSuccess {Object[]} appointments   Array of appointment objects
+  * @apiSuccess {Number} appointments.appointment_slot_id   Unique ID of appointment slot
+  * @apiSuccess {Date} appointments.date  Date of appointment
+  * @apiSuccess {String} appointments.start_time   Start time of appointment
+  * @apiSuccess {String} appointments.end_time   End time of appointment
+  * @apiSuccess {String} appointments.appointment_type   Type of appointment
+      ("shopping" or "new bed")
+  * @apiSuccess {String} appointments.delivery_method   "delivery" or "pickup"
+  * @apiSuccess {String} appointments.location_name   Name of location
+      ("Bloomington" or "Roseville")
+  * @apiSuccess {String} appointments.location_address   Address of location
+
   * @apiSuccessExample {json} Success-Response:
       HTTP/1.1 200 OK
       [{
         "appointment_slot_id": 3,
-        "appointment_date": "June 9, 2017",
+        "date": "June 9, 2017",
         "start_time": "9:15 am",
         "end_time": "10:15 am",
         "appointment_type": "shopping",
@@ -65,8 +102,49 @@ router.get('/existing/:user_id', {
   * @apiErrorExample {json} List error
   *    HTTP/1.1 500 Internal Server Error
 */
-router.get('/available', {
+router.get('/available', function(req, res) {
   // TODO: add code
-})
+});
+
+/**
+  * @api {post} /appointments/reserve Make Appointment
+  * @apiVersion 0.1.0
+  * @apiName MakeAppointment
+  * @apiGroup Appointments
+  * @apiDescription Makes appointment & saves to database
+
+  * @apiParam {Date} date   Date of appointment
+  * @apiParam {String} start_time   Start time of appointment
+  * @apiParam {String} end_time   End time of appointment
+  * @apiParam {Number} user_id   Unique id of user creating the appointment
+  * @apiParam {Number} client_id   Unique id of client for whom appointment was created
+  * @apiParam {Number} appointment_slot_id   Unique id of appointment slot
+  * @apiParam {Date} appointment_date_added   Date on which appointment was created (current date)
+  * @apiParam {String} status   Whether appointment is confirmed, canceled, or pending (should pass in pending)
+  * @apiSuccess {Number} appointment_id   Unique ID of appointment that has been created
+  * @apiErrorExample {json} List error
+  *    HTTP/1.1 500 Internal Server Error
+*/
+router.post('/reserve', function(req, res) {
+  // TODO: add code
+});
+
+/**
+  * @api {put} /appointments/existing Update Appointment
+  * @apiVersion 0.1.0
+  * @apiName UpdateAppointment
+  * @apiGroup Appointments
+  * @apiDescription Updates status of an appointment in database
+
+  * @apiParam {Number} appointment_id  Unique ID of appointment
+  * @apiParam {String} status  Status of appointment ("confirmed", "pending", or "canceled")
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  * @apiErrorExample Not found error
+  *    HTTP/1.1 404 Not found
+*/
+router.put('/existing', function(req, res) {
+  // TODO: add code
+});
 
 module.exports = router;
---
+++ server/routes/rules.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+/**
+  * @api {get} /rules/zip/:zip_code Get locations for ZIP code
+  * @apiVersion 0.1.0
+  * @apiName GetZipLocation
+  * @apiGroup Rules
+  * @apiDescription Determines which location(s) should be available to a user
+  *    given the client's ZIP code
+  *
+  * @apiParam {Number} zip_code   ZIP code of client seeking appointment
+  * @apiSuccess {Object[]} locations   Array of location objects representing
+  *    the Bridging locations at which the user is permitted make an appointment
+  * @apiSuccess {Object} locations.location   Object containing the location's name and address
+  * @apiSuccess {String} locations.location.name   Name of location ("Bloomington
+  *    or Roseville")
+  * @apiSuccess {String} locations.location.address   Address of location
+  * @apiSuccessExample {json} Success-Response:
+  *    HTTP/1.1 200 OK
+  *    [
+  *      {
+  *        "name": "Roseville",
+  *        "address": "1730 Terrace Dr, Roseville, MN",
+  *      }
+  *    ]
+  * @apiErrorExample {json} List error
+  *    HTTP/1.1 500 Internal Server Error
+*/
+router.get('/zip/:zip_code', function(req, res) {
+
+});
+
+module.exports = router;
