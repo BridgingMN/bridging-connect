18522f82dd4c805082c7b1a77a7a6c2727025cb4
Anna Springfield <anna.e.springfield@gmail.com>
2017-05-19 11:40:48 -0500

    update query returns

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -161,7 +161,7 @@ router.post('/reserve', function(req, res) {
       db.query('INSERT INTO "appointments" ("appointment_slot_id", "user_id", \
        "client_id", "created_date", "appointment_date", "status_id")  \
       VALUES ($1, $2, $3, $4, $5, (SELECT "id" FROM "statuses" \
-      WHERE "status" = $6) RETURNING "id")',
+      WHERE "status" = $6)) RETURNING "id"',
       [appointment_slot_id, user_id, client_id, created_date, appointment_date, status],
       function(queryError){
         done();
