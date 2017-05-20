a293061da4330d4c3c05134c1619e34df2026d08
Betsy Rowley <BRowley8@gmail.com>
2017-05-20 12:12:06 -0500

    Adds all agencies to the drop down in new caseworker form

--- client/scripts/controllers/AdminCaseworkerNewController.js
+++ client/scripts/controllers/AdminCaseworkerNewController.js
@@ -4,29 +4,15 @@ angular
       function(UserService, $http, $location) {
   // DATA-BINDING VARIABLES
   var vm = this; // controller reference
-  vm.caseworker = {
-    user_type: 'casworker',
-    agency: '',
-    bridging_agency_id: '',
-    first: '',
-    last: '',
-    department: null,
-    street: null,
-    city: null,
-    state: null,
-    zip_code: null,
-    day_phone: null,
-    ext: null,
-    email: '',
-    notes: '',
-    access_disabled: false
-  };
+  vm.agencies = UserService.agencies;
+  vm.getAgencies = UserService.getAgencies;
+  vm.caseworker = {};
 
   vm.createCaseworker = createCaseworker;
 
   //Creates a new Agency
   function createCaseworker() {
-    console.log('submit clicked', vm.caseworker);
+    console.log('Create Caseworker clicked', vm.caseworker);
     $http.post('/caseworkers', vm.caseworker).then(function(response) {
       console.log(response);
       $location.path('/admin-caseworker-overview');
