7da15803a3575ef34ecda7706f48f5ff496b396a
Betsy Rowley <BRowley8@gmail.com>
2017-05-23 14:10:02 -0500

    Posts new Default Apt slot to the db

--- client/scripts/controllers/AdminDefaultAppointmentNewController.js
+++ client/scripts/controllers/AdminDefaultAppointmentNewController.js
@@ -1,7 +1,7 @@
 angular
   .module('myApp')
-  .controller('AdminDefaultAppointmentNewController', ['UserService',
-      function(UserService) {
+  .controller('AdminDefaultAppointmentNewController', ['$http', '$location', 'UserService',
+      function($http, $location, UserService) {
   // DATA-BINDING VARIABLES
   var vm = this; // controller reference
   vm.locations = UserService.locations;
@@ -13,12 +13,25 @@ angular
   vm.methods = UserService.methods;
   vm.getMethods = UserService.getMethods;
   vm.init = init;
+  vm.default = {};
+  vm.createDefault = createDefault;
 
+//Initializes all get routes to populate the form for creating a
+//default appointment slot
   function init() {
     vm.getLocations();
     vm.getDays();
     vm.getTypes();
     vm.getMethods();
+  }
+
+  //Creates/Posts a default appointment slot
+  function createDefault() {
+    console.log('Button to create default clicked: ', vm.default);
+    $http.post('/schedule/default', vm.default).then(function() {
+      $location.path('/admin-appointments-default');
+      alert('The new DEFAULT Appointment Slot has been added.');
+    });
   }
 
 }]);
