fa491849ac556ca12854ccc609c97598d2a57b54
Betsy Rowley <BRowley8@gmail.com>
2017-05-23 11:50:55 -0500

    Adds variables to controller

--- client/scripts/controllers/AdminDefaultAppointmentNewController.js
+++ client/scripts/controllers/AdminDefaultAppointmentNewController.js
@@ -4,6 +4,14 @@ angular
       function(UserService) {
   // DATA-BINDING VARIABLES
   var vm = this; // controller reference
-  vm.hello = 'Hello in new Appointments-Default!';
+  vm.locations = UserService.locations;
+  vm.getLocations = UserService.getLocations;
+  vm.days = UserService.days;
+  vm.getDays = UserService.getDays;
+  vm.types = UserService.types;
+  vm.getTypes = UserService.getTypes;
+  vm.methods = UserService.methods;
+  vm.getMethods = UserService.getMethods;
+  
 
 }]);
