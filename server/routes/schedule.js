2981a3e60ec5bdc79956916f2e2ce395c2366537
Dan Zera <dczera@gmail.com>
2017-05-23 11:18:31 -0500

    add GET route for appointment_types

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,7 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../modules/database.js');
+var formatters = require('../modules/formatters.js');
+var formatTimeForPostgres = formatters.formatTimeForPostgres;
 
+
+router.get('/types', function(req, res) {
+  if (req.isAuthenticated()) { // user is authenticated
+    pool.connect(function(err, database, done) {
+      if (err) { // connection error
+        console.log('error connecting to the database:', err);
+      } else { // we connected
+        database.query('SELECT "appointment_type" FROM "appointment_types";',
+          function(queryErr, result) { // query callback
+            done();
+            if (queryErr) {
+              console.log('error making query:', queryErr);
+              res.sendStatus(500);
+            } else {
+              console.log('sucessful get from /schedule/types', result.rows);
+              var appointment_types = result.rows.map(function(apptTypeObj) {
+                return apptTypeObj.appointment_type;
+              });
+              console.log('appointment types formatted:', appointment_types);
+              res.send(appointment_types);
+            }
+        }); // end query callback
+      } // end DB connection if-else
+    }); // end pool.connect
+  } else { // user not authenticated
+    res.sendStatus(401);
+  }
+});
 
 /**
   * @api {post} /schedule Add a New Appointment Slot
@@ -10,15 +40,13 @@ var pool = require('../modules/database.js');
   * @apiGroup Appointment Schedule
   * @apiDescription Adds a new appointment slot to the "appointment_slots" table in the database.
   *
-  * @apiParam {Number} agency_id Mandatory Unique ID of the agency the caseworker is associated with.
-  * @apiParam {String} first First name of the caseworker from the "users" table.
-  * @apiParam {String} last Last name of the caseworker from the "users" table.
-  * @apiParam {String} day_phone Daytime phone number of the caseworker from the "users" table.
-  * @apiParam {String} ext Phone number extension of caseworker from the "users" table.
-  * @apiParam {String} email Mandatory Email address of caseworker from the "users" table.
-  * @apiParam {Boolean} access_disabled Mandatory Current caseworker status. True = access disabled.
-  * @apiParam {String} notes Any notes the administrator leaves regarding a caseworker.
-  * @apiParam {String} user_type Mandatory Indicator for the type of user being created. Corresponds to an entry in the "user_types" table.
+  * @apiParam {String} appointment_type Mandatory Type of appointment corresponding to an entry in the "appointment_types" table.
+  * @apiParam {String} day Mandatory Day name corresponding to an entry in the "days" table.
+  * @apiParam {String} delivery_method Mandatory Delivery method name corresponding to an entry in the "delivery_methods" table.
+  * @apiParam {String} location Mandatory Location name corresponding to an entry in the "locations" table.
+  * @apiParam {Time} start_time Mandatory Start time for the new appointment slot.
+  * @apiParam {Time} end_time Mandatory End time for the new appointment slot.
+  * @apiParam {Number} num_allowed Mandatory Number of appointments allowed for the new appointment slot.
   *
   * @apiSuccess {Number} id Unique ID of the new caseworker.
   *
@@ -28,23 +56,23 @@ var pool = require('../modules/database.js');
 router.post('/', function(req, res) {
   console.log('in the post route for creating caseworker', req.body);
   if (req.isAuthenticated()) { // user is authenticated
-    var agency_id = req.body.agency_id;
-    var first = req.body.first;
-    var last = req.body.last;
-    var day_phone = req.body.day_phone || null;
-    var ext = req.body.ext || null;
-    var email = req.body.email;
-    var access_disabled = req.body.access_disabled || false;
-    var notes = req.body.notes || null;
-    var user_type = 'caseworker';
+    var appointment_type = req.body.appointment_type;
+    var day = req.body.day;
+    var delivery_method = req.body.delivery_method;
+    var location = req.body.location;
+    var start_time = formatTimeForPostgres(req.body.start_time);
+    var end_time = formatTimeForPostgres(req.body.end_time);
+    var num_allowed = req.body.num_allowed;
     pool.connect(function(err, database, done) {
       if (err) { // connection error
         console.log('error connecting to the database:', err);
         res.sendStatus(500);
       } else { // we connected
-        database.query('INSERT INTO "users" ("agency_id", "first", "last", "day_phone", "ext", "email", "access_disabled", "notes", "user_type_id") ' +
-                        'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, ' +
-                        '(SELECT "id" FROM "user_types" WHERE "user_type" = $9));',
+        database.query('INSERT INTO "appointment_slots" ("appointment_type_id", "day_id", "delivery_method_id", "location_id", "start_time", "end_time", "num_allowed") ' +
+                        'VALUES ((SELECT "id" FROM "appointment_types" WHERE "appointment_type" = $$$$), ' +
+                        '(SELECT "id" FROM "days" WHERE "name" = $$$$), ' +
+                        '(SELECT "id" FROM "delivery_method_id" WHERE "name" = $$$$), ' +'
+                        ');',
                         [agency_id, first, last, day_phone, ext, email, access_disabled, notes, user_type],
           function(queryErr, result) { // query callback
             done(); // release connection to the pool
