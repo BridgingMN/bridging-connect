e3f83b8f2004da18c40ba4b6a50a65464f5cb51d
Nic Wilson <nicwilson@gmail.com>
2017-05-24 13:52:29 -0500

    Refactor Reschedule Appointment

--- client/scripts/controllers/CaseworkerAppointmentsAllController.js
+++ client/scripts/controllers/CaseworkerAppointmentsAllController.js
@@ -14,6 +14,20 @@ angular
   vm.user = UserService.userObject;
   vm.CONSTANTS = constants;
 
+
+  var todaysDate = new Date();
+  //Limits for the range of dates on the calendar
+  vm.minDate = new Date(
+    todaysDate.getFullYear(),
+    todaysDate.getMonth(),
+    todaysDate.getDate() + 3
+  );
+  vm.maxDate = new Date(
+    todaysDate.getFullYear(),
+    todaysDate.getMonth() + 3,
+    todaysDate.getDate()
+  );
+
   // Stores current appointment filter, by default should show all appointments
   vm.appointmentFilter = '';
 
@@ -70,13 +84,13 @@ angular
   function rescheduleAppointment(appointment) {
     console.log('Rescheduling', appointment);
     UserService.newAppointment = new AppointmentService.Appointment(appointment.info.appointment_type);
-    UserService.newAppointment.appointment.date = appointment.info.appointment_date;
+    UserService.newAppointment.appointment.date = new Date(appointment.info.appointment_date);
     UserService.newAppointment.appointment.start_time = appointment.info.start_time;
     UserService.newAppointment.client_id = appointment.client.client_id;
-    UserService.newAppointment.delivery_method = appointment.info.delivery_type;
+    UserService.newAppointment.delivery_method = appointment.info.delivery_method;
     UserService.newAppointment.location_id = appointment.info.location_id;
     UserService.newAppointment.location_name = appointment.info.location_name;
-    $location.path('/caseworker-appointment-reschedule');
+    getAvailableAppointments(vm.minDate, vm.maxDate);
   }
 
   //This function will redirect the caseworker to a view where they can edit the referall form for their client
@@ -136,5 +150,38 @@ angular
   function createNewAppointment() {
     console.log('Resetting Appointment object');
     //This function should call a method in the service that resets the newAppointment object and redirects the caseworker to the new appointment view
+  }
+
+  /**
+   * Sends a request to the server to get available appointments.
+   * @function getAvailableAppointments
+   * @param {date} min_date Lowerbound of date range for query
+   * @param {date} max_date Upperbound of date range for query
+   */
+  function getAvailableAppointments(min_date, max_date) {
+    console.log('Getting Appointments', min_date, max_date);
+    UserService.newAppointment.getAvailableAppointments(min_date, max_date)
+      .then(availableAppointmentsSuccess, availableAppointmentsError);
+  }
+
+  /**
+  * Called upon receiving an array of available Appointment objects
+  * Updates the view to reflect available Appointments
+  * Sets the currently selected Appointment to the first appointment in the array
+  * @function availableAppointmentsSuccess
+  * @param {array} response Array of Appointment objects
+  */
+  function availableAppointmentsSuccess(response) {
+    AppointmentService.availableAppointments = response;
+    $location.path('/caseworker-appointment-reschedule');
+  }
+
+  /**
+  * Handles error response from getAvailableAppointments
+  * @function availableAppointmentsError
+  * @param {object} error Error response.
+  */
+  function availableAppointmentsError(error) {
+    console.error(error);
   }
 }]);
