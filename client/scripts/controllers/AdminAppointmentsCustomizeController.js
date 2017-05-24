3035e51b13d02a9aa2d74b815d42c1af2e4d8bd2
Betsy Rowley <BRowley8@gmail.com>
2017-05-24 16:09:28 -0500

    Adds customize override form

--- client/scripts/controllers/AdminAppointmentsCustomizeController.js
+++ client/scripts/controllers/AdminAppointmentsCustomizeController.js
@@ -3,21 +3,35 @@ angular
   .controller('AdminAppointmentsCustomizeController', ['UserService', function(UserService) {
   // DATA-BINDING VARIABLES
   var vm = this; // controller reference
-  // vm.hello = "Hello in Appointments-Customize!";
+  vm.locations = UserService.locations;
+  vm.getLocations = UserService.getLocations;
+  vm.types = UserService.types;
+  vm.getTypes = UserService.getTypes;
+  vm.methods = UserService.methods;
+  vm.getMethods = UserService.getMethods;
+  vm.init = init;
+
+  //Initializes all get routes to populate the form for creating a
+  //customized appointment slot
+    function init() {
+      vm.getLocations();
+      vm.getTypes();
+      vm.getMethods();
+    }
 
 
 //for location selector
-  vm.locations = ['Bloomington', 'Roseville'];
-  vm.selectedLocation;
-  vm.getSelectedLocation = function() {
-     if (vm.selectedLocation !== undefined) {
-       return 'You have selected: ' + vm.selectedLocation;
-     } else {
-       return 'Please select an location.';
-     }
-   };
+  // vm.locations = ['Bloomington', 'Roseville'];
+  // vm.selectedLocation;
+  // vm.getSelectedLocation = function() {
+  //    if (vm.selectedLocation !== undefined) {
+  //      return 'You have selected: ' + vm.selectedLocation;
+  //    } else {
+  //      return 'Please select an location.';
+  //    }
+  //  };
 //for datepicker
-   vm.myDate = new Date();
-   vm.isOpen = false;
+  //  vm.myDate = new Date();
+  //  vm.isOpen = false;
 
 }]);
