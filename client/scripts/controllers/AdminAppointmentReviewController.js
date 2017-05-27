6478928e29cfea9821425e6517958038f56048ca
Betsy Rowley <BRowley8@gmail.com>
2017-05-27 10:52:05 -0500

    updating confirm apt function

--- client/scripts/controllers/AdminAppointmentReviewController.js
+++ client/scripts/controllers/AdminAppointmentReviewController.js
@@ -35,6 +35,7 @@ vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA ' +
 
   function confirmApt(appointment) {
     console.log('Confirming Appointment: ', appointment);
+    if(appointment.delivery_date === null)
     $http.put('/appointments/update/', + 'confirmed', appointment).then(function() {
       $location.path('/admin-appointments-all');
       alert('The appointment has been confirmed and a confirmation email has ' +
