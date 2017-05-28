4d4b12445c61aa3d5672b149c577f0a1d65d070e
Betsy Rowley <BRowley8@gmail.com>
2017-05-28 10:21:27 -0500

    Creates data table for all override dates on the admin appointments customize view

--- client/scripts/controllers/AdminAppointmentsCustomizeController.js
+++ client/scripts/controllers/AdminAppointmentsCustomizeController.js
@@ -1,37 +1,19 @@
 angular
   .module('myApp')
-  .controller('AdminAppointmentsCustomizeController', ['UserService', function(UserService) {
+  .controller('AdminAppointmentsCustomizeController', ['$http', 'UserService',
+      function($http, UserService) {
   // DATA-BINDING VARIABLES
   var vm = this; // controller reference
-  vm.locations = UserService.locations;
-  vm.getLocations = UserService.getLocations;
-  vm.types = UserService.types;
-  vm.getTypes = UserService.getTypes;
-  vm.methods = UserService.methods;
-  vm.getMethods = UserService.getMethods;
-  vm.init = init;
+  vm.query = UserService.query;
+  vm.overrides = {};
+  vm.getAllOverrides = getAllOverrides;
 
-  //Initializes all get routes to populate the form for creating a
-  //customized appointment slot
-    function init() {
-      vm.getLocations();
-      vm.getTypes();
-      vm.getMethods();
-    }
-
-
-//for location selector
-  // vm.locations = ['Bloomington', 'Roseville'];
-  // vm.selectedLocation;
-  // vm.getSelectedLocation = function() {
-  //    if (vm.selectedLocation !== undefined) {
-  //      return 'You have selected: ' + vm.selectedLocation;
-  //    } else {
-  //      return 'Please select an location.';
-  //    }
-  //  };
-//for datepicker
-  //  vm.myDate = new Date();
-  //  vm.isOpen = false;
+  function getAllOverrides() {
+    console.log('client request to server for all override slots');
+    $http.get('/overrides').then(function(response) {
+      vm.overrides.array =response.data;
+      console.log('All Overrides: ', vm.overrides.array);
+    });
+  }
 
 }]);
